5e9edd80f1958cb444b6aa3d60cd4b742dbf078a
Charnaye Grier <charnayegrier@gmail.com>
2022-09-30 13:20:03 -0400

    created Context (from React) to begin logic for the store

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -4,11 +4,13 @@ import { Home } from "./pages/Home"
 import { Donate } from "./pages/Donate"
 import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
 
   return (
     <div>
+      <ShoppingCartProvider>
     <Navbar />
     <Container className="mb-4">
       <Routes>
@@ -17,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </Container>
+    </ShoppingCartProvider>
     </div>
   )
 }
